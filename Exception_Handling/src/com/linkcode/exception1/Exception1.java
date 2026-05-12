f377ca24a46c6a03fc4981509cea2c38cb4e2e8b
yogeshyedave-111 <yogeshyedave268@gmail.com>
2026-05-12 15:26:29 +0530

    initial commit

---
+++ Exception_Handling/src/com/linkcode/exception1/Exception1.java
@@ -0,0 +1,19 @@
+package com.linkcode.exception1;
+//import java.io.FileInputStream;
+public class Exception1 {
+public static void main(String[] args) {
+	System.out.println("main start");
+	try{
+		int a=10/0;
+		}catch(ArithmeticException obj) {
+			
+		}catch(Exception b) {
+			
+		}
+	
+	
+	System.out.println("main end");
+	
+	
+}
+}
